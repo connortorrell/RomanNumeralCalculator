1f295c759985ed8c91a64b6e63461d34e987f817
Connor Torrell <connortorrell@gmail.com>
2020-11-21 15:19:59 -0500

    challenge start

--- RomanNumeralCalculator.rb
+++ RomanNumeralCalculator.rb
@@ -1,6 +1,20 @@
 class RomanNumeralCalculator
-  def add(operand_1, operand_2)
-    "II"
+
+  def to_decimal(roman_number)
+    
+
+
+  end
+
+  def to_roman(decimal_number)
+    
+  end
+
+  def add(roman_operand_1, roman_operand_2)
+    decimal_operand_1 = to_decimal(roman_operand_1)
+    decimal_operand_2 = to_decimal(roman_operand_2)
+    decimal_sum = decimal_operand_1 + decimal_operand_2
+    to_roman(decimal_sum)
   end
 
 end
--- spec/RomanNumeralCalculator_spec.rb
+++ spec/RomanNumeralCalculator_spec.rb
@@ -9,6 +9,14 @@ describe "RomanNumeralCalculator" do
     it "gives II for I + I" do
       expect(calculator.add("I", "I")).to eq("II")
     end
+    it "gives III for II + I" do
+      expect(calculator.add("II", "I")).to eq("III")
+    end
+    it "gives IV for III + I"
+    it "gives V for IV + I"
+    it "gives X for IX + I"
+    it "gives XIX for X + IX" 
+    
   end
 
 end
