4174dc31c54f9bce907e3c85bb7f756cfdc8a17f
Connor Torrell <connortorrell@gmail.com>
2020-11-23 20:17:33 -0500

    added functionality for adding and subtracting

--- RomanNumeralCalculator.rb
+++ RomanNumeralCalculator.rb
@@ -1,19 +1,63 @@
 class RomanNumeralCalculator
 
+  $roman_values = Hash["I" => 1, "IV" => 4, "V" => 5, "IX" => 9, "X" => 10, 
+    "XL" => 40, "L" => 50, "XC" => 90, "C" => 100, 
+    "CD" => 400, "D" => 500, "CM" => 900, "M" => 1000]
+
   def to_decimal(roman_number)
-    
-
-
+    reversed_roman_number = roman_number.reverse.upcase
+    sum = 0
+    i = 0
+    while i < roman_number.length
+      current_roman_numeral = $roman_values[reversed_roman_number[i]]
+      previous_roman_numeral = $roman_values[reversed_roman_number[i - 1]]
+      if i == 0
+        sum += current_roman_numeral
+      else
+        if current_roman_numeral >= previous_roman_numeral
+          sum += current_roman_numeral
+        else
+          sum -= current_roman_numeral
+        end
+      end
+      i += 1
+    end
+    sum
   end
 
   def to_roman(decimal_number)
-    
+    if $roman_values.key(decimal_number) != nil
+      $roman_values.key(decimal_number)
+    else
+      roman_value = ""
+      decimal_value = decimal_number
+      until decimal_value == 0
+        i = 0
+        while i < $roman_values.length
+          if $roman_values.values[i] > decimal_value
+            decimal_value -= $roman_values.values[i - 1]
+            roman_value += $roman_values.keys[i - 1]
+            i = $roman_values.length
+          else
+            i += 1
+          end
+        end
+      end
+      roman_value
+    end
   end
 
   def add(roman_operand_1, roman_operand_2)
     decimal_operand_1 = to_decimal(roman_operand_1)
     decimal_operand_2 = to_decimal(roman_operand_2)
     decimal_sum = decimal_operand_1 + decimal_operand_2
+    to_roman(decimal_sum)
+  end
+
+  def subtract(roman_operand_1, roman_operand_2)
+    decimal_operand_1 = to_decimal(roman_operand_1)
+    decimal_operand_2 = to_decimal(roman_operand_2)
+    decimal_sum = decimal_operand_1 - decimal_operand_2
     to_roman(decimal_sum)
   end
 
--- spec/RomanNumeralCalculator_spec.rb
+++ spec/RomanNumeralCalculator_spec.rb
@@ -1,7 +1,5 @@
 require './RomanNumeralCalculator'
 
-
-
 describe "RomanNumeralCalculator" do
 
   let(:calculator) { RomanNumeralCalculator.new }
@@ -12,11 +10,28 @@ describe "RomanNumeralCalculator" do
     it "gives III for II + I" do
       expect(calculator.add("II", "I")).to eq("III")
     end
-    it "gives IV for III + I"
-    it "gives V for IV + I"
-    it "gives X for IX + I"
-    it "gives XIX for X + IX" 
-    
+    it "gives IV for III + I" do
+      expect(calculator.add("III", "I")).to eq("IV")
+    end
+    it "gives V for IV + I" do
+      expect(calculator.add("IV", "I")).to eq("V")
+    end
+    it "gives X for IX + I" do
+      expect(calculator.add("IX", "I")).to eq("X")
+    end
+    it "gives XIX for X + IX" do
+      expect(calculator.add("X", "IX")).to eq("XIX")
+    end
+    it "gives CDLXIV for XLI + CDXXIII" do
+      expect(calculator.add("XLI", "CDXXIII")).to eq("CDLXIV")
+    end
   end
-
+  context "subtracting" do
+    it "gives III for IX - VI" do
+      expect(calculator.subtract("IX", "VI")).to eq("III")
+    end
+    it "gives XVI for XX - IV" do
+      expect(calculator.subtract("XX", "IV")).to eq("XVI")
+    end
+  end
 end
